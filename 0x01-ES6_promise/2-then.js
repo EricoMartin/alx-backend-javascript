ddc1a36465882e9ae33d560adfac58874649f739
EricoMartin <ericomartin.0785@gmail.com>
2024-05-18 11:39:16 +0000

    2-then.js

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,24 @@
+export default function handleResponseFromAPI(promise) {
+  return new Promise((resolve, reject) => {
+    if (promise) {
+      resolve(promise);
+    }
+  });
+}
+
+function resolvePromise() {
+  const myobj = {
+        status: 200,
+        body: "success",
+  }
+  return myobj;
+}
+
+function rejectPromise() {
+  return new Error();
+}
+
+function logResponse() {
+  console.log('Got a response from the API');
+}
+handleResponseFromAPI(resolvePromise).then(logResponse).catch(rejectPromise);
