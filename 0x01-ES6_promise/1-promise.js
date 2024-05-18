74c9287678b33f8ba149ab473fff06f0fa106282
EricoMartin <ericomartin.0785@gmail.com>
2024-05-18 01:22:11 +0000

    1-promise.js

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,13 @@
+export default getFullResponseFromAPI(success) {
+  return new Promise(resolve, reject) {
+    if (!success) {
+      reject(new Error("The fake API is not working currently"));
+    }
+     const myobj = 
+	{
+	  "status": 200,
+	  "body": 'Success'
+	}
+      resolve(myobj);
+  }
+}
