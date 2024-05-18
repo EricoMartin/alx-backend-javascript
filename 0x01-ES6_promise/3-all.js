8bb1cb7e9fd047d97b3878cd69aa6e404ef58fa0
EricoMartin <ericomartin.0785@gmail.com>
2024-05-18 13:42:54 +0000

    3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,5 +4,5 @@ export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then((res) => {
       console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
-    }).catch(() => new Error('Signup system offline'));
+    }).catch(() => { console.log('Signup system offline'); });
 }
