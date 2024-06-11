cd1f5fb1a3605da835762e1ee13c9b52cd6784c8
EricoMartin <ericomartin.0785@gmail.com>
2024-06-11 17:05:04 +0000

    stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,14 +1,13 @@
-//A process commandline task
+// A process commandline task
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdout.write("Welcome to Holberton School, what is your name?\n");
-
-process.stdin.on('readable', function() {
-  var username = process.stdin.read();
+process.stdin.on('readable', () => {
+  const username = process.stdin.read();
   if (username !== null) {
-    process.stdout.write('Your name is: ' + username);
+    process.stdout.write(`Your name is: ${username}`);
   }
 });
 
-process.stdin.on('end', function() {
+process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
