9314893ac7f5ab9c6bc5c28b15920fdd976e6568
EricoMartin <ericomartin.0785@gmail.com>
2024-06-11 16:21:13 +0000

    stdin

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,15 @@
+//A process commandline task
+
+process.stdout.write("Welcome to Holberton School, what is your name?\n");
+process.stdin.setEncoding('utf8');
+
+process.stdin.on('readable', function() {
+  var username = process.stdin.read();
+  if (username !== null) {
+    process.stdout.write('Your name is: ' + username);
+  };
+});
+
+process.on('exit', function() {
+  console.log('This important software is now closing\n');
+});
